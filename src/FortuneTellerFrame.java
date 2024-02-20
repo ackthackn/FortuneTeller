fa1fa8bf1d5784ea44e561067057b141003d221c
Ethan <white2e9@mail.uc.edu>
2024-02-19 21:05:47 -0500

    finished the middle panel

--- src/FortuneTellerFrame.java
+++ src/FortuneTellerFrame.java
@@ -1,37 +1,65 @@
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
 import java.awt.*;
 
 public class FortuneTellerFrame extends JFrame {
-
-    private ImageIcon fortuneTellerImage;
-    private JLabel displayImage;
+    JFrame frame;
+    JPanel panelTop;
+    JPanel panelMiddle;
+    JPanel panelBottom;
+    JLabel fortuneLabel;
+    ImageIcon fortuneTellerImage;
+    JLabel displayImage;
+    JTextArea textArea;
+    JScrollPane scrollPane;
     public FortuneTellerFrame() {
-        JFrame frame = new JFrame();
+        frame = new JFrame();
         frame.setSize(600,600);
         frame.setTitle("Fortune Teller");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+//----------------------------------------------------------------------------
+        panelTop = new JPanel();
 
-        JPanel panelTop = new JPanel();//new GridLayout(1,2));
-
-
-        JLabel fortuneLabel = new JLabel("Fortune Teller");
+        fortuneLabel = new JLabel("Fortune Teller");
         fortuneLabel.setFont(new Font("Bookman Old Style", Font.PLAIN, 36));
 
         fortuneTellerImage = new ImageIcon("fortuneTeller.png");
         displayImage = new JLabel(fortuneTellerImage);
 
-        // set locations
         fortuneLabel.setBounds(180, 10, 300, 50);
         displayImage.setBounds(260,70,100, 100);
 
-        // add stuff
+//----------------------------------------------------------------------------
+        panelMiddle = new JPanel();
+
+        textArea = new JTextArea(15,30);
+        textArea.setEditable(false);
+        scrollPane = new JScrollPane(textArea);
+
+//----------------------------------------------------------------------------
+        panelBottom = new JPanel();
+
+// add stuff -----------------------------------------------------------------
         frame.add(panelTop);
+        frame.add(panelMiddle);
+        frame.add(panelBottom);
+
         panelTop.add(fortuneLabel);
         panelTop.add(displayImage);
 
+        panelMiddle.add(scrollPane);
+
+// layout stuff --------------------------------------------------------------
         panelTop.setLayout(null);
-        panelTop.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+        panelMiddle.setLayout(null);
+        panelBottom.setLayout(null);
+
+        panelTop.setBounds(0,0,600,200);
+        panelMiddle.setBounds(0,200,600,200);
+        panelBottom.setBounds(0,400,600,200);
     }
 
     public static void main(String[] args) {
